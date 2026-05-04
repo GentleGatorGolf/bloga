749a89cbdf5da875199c367b1c47001d7bd12cf1
hoyt <hoytdawson@gmail.com>
2026-05-03 20:05:31 -0400

    landing page 3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,7 +9,7 @@ export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    component: LandingPage2Component,
+    component: LandingPage3Component,
     title: 'Welcome to Gentle Gator Golf Training'
   },
   {
